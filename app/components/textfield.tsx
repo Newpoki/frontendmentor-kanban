91c12581517215ca2cf413d2020642149cc03f24
Jason Savelli <jason.savelli@mention.com>
2023-10-21 23:39:46 +0200

    feat: add textfield onDelete prop

--- app/components/textfield.tsx
+++ app/components/textfield.tsx
@@ -1,33 +1,52 @@
 import { twMerge } from 'tailwind-merge';
 import { FieldLabel } from './field-label';
+import Delete from '@/public/assets/delete.svg';
 
 type Props = React.HTMLAttributes<HTMLInputElement> & {
     error?: string;
     inputClassName?: string;
     label?: string;
+    onDelete?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-export const TextField = ({ className, error, id, inputClassName, label, ...others }: Props) => {
+export const TextField = ({
+    className,
+    error,
+    id,
+    inputClassName,
+    label,
+    onDelete,
+    ...others
+}: Props) => {
     return (
         <div className={twMerge('flex flex-col gap-2', className)}>
             {label != null && <FieldLabel htmlFor={id}>{label}</FieldLabel>}
 
-            <div
-                className={twMerge(
-                    'border-1 flex items-center gap-2 rounded-[4px] border-solid border-grey500/25 px-4 py-2 text-body-l transition-colors focus-within:border-grey500 hover:border-grey500',
-                    error != null && 'border-red500 focus-within:border-red500 hover:border-red500'
-                )}
-            >
-                <input
-                    {...others}
+            <div className="flex w-full items-center gap-4">
+                <div
                     className={twMerge(
-                        'bg-transparent  outline-none placeholder:text-grey500/25',
-
-                        inputClassName
+                        'border-1 flex w-full items-center gap-2 rounded-[4px] border-solid border-grey500/25 px-4 py-2 text-body-l transition-colors focus-within:border-grey500 hover:border-grey500',
+                        error != null &&
+                            'border-red500 focus-within:border-red500 hover:border-red500'
                     )}
-                    id={id}
-                />
-                {error != null && <span className=" text-red500">{error}</span>}
+                >
+                    <input
+                        {...others}
+                        className={twMerge(
+                            'bg-transparent  outline-none placeholder:text-grey500/25',
+
+                            inputClassName
+                        )}
+                        id={id}
+                    />
+                    {error != null && <span className=" text-red500">{error}</span>}
+                </div>
+
+                {onDelete && (
+                    <button>
+                        <Delete className="w-4 text-grey500" />
+                    </button>
+                )}
             </div>
         </div>
     );
