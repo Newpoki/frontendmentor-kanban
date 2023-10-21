6c4e6ae271119953038e48d03582ba8812154985
Jason Savelli <jason.savelli@mention.com>
2023-10-21 23:17:08 +0200

    task: dialog component max height and width

--- app/components/dialog/dialog.tsx
+++ app/components/dialog/dialog.tsx
@@ -15,7 +15,9 @@ export const Dialog = ({ isOpen, onClose, children }: DialogProps) => {
             onClose={onClose}
             isDisplayed={isOpen}
         >
-            <div className="flex flex-col gap-6 rounded-md bg-white p-6">{isOpen && children}</div>
+            <div className="flex max-h-[calc(100dvh-32px)] w-[calc(100dvw-32px)] flex-col gap-6 rounded-md bg-white p-6">
+                {isOpen && children}
+            </div>
         </Backdrop>
     );
 };
