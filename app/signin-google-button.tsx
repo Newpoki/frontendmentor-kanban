044f1ff12a9b588bf1742f606649ec810aa2a925
Jason Savelli <jason.savelli@mention.com>
2023-10-21 17:52:52 +0200

    feat: sign in button now works

--- app/signin-google-button.tsx
+++ app/signin-google-button.tsx
@@ -1,10 +1,20 @@
 'use client';
 
 import LogoGoogle from '@/public/assets/logo-google.svg';
+import { signIn } from 'next-auth/react';
+import { useCallback } from 'react';
 
 export const SigninGoogleButton = () => {
+    const handleClick = useCallback(() => {
+        signIn('google');
+    }, []);
+
     return (
-        <button className="flex items-center justify-center gap-3 rounded-lg bg-white px-4 py-4 shadow-md hover:bg-grey100">
+        <button
+            className="flex items-center justify-center gap-3 rounded-lg bg-white px-4 py-4 shadow-md hover:bg-grey100"
+            type="button"
+            onClick={handleClick}
+        >
             <LogoGoogle className="w-5" />
             <span className="text-body-m">Sign in with Google</span>
         </button>
